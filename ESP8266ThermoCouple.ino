25567004f41546777b28c4f856dc416974f6a95d
Kentaro Urata <kentu@microsoft.com>
2021-02-15 03:06:58 -0800

    single voltage divider design

--- ESP8266ThermoCouple.ino
+++ ESP8266ThermoCouple.ino
@@ -1,54 +1,87 @@
 #include <math.h>
-#include <SimpleDHT.h>
 
 #define therm_pin A0
 
 double V_0 = 3.291;
-double R_1 = 100000.0; VCC - R_1 - A0 + Thermistor - GND
+
+// WeMos D1 Mini internally contains voltage divider for A0 input in order to expand the voltage
+// range of ESP8266 which is limited to 1.0V
+// R_2 of the divider (between A0 and GND) is fixed 100k ohm
+double R_2 = 100000.0; //VCC - R_1 - A0 - R_2 - GND (R_1 = 230 + rThermistor)
+
+// D1 Mini also has 220k ohm on the VCC side of the divider
+double R_1d = 220000.0;
+
+// I'm adding additional 10k register on VCC side of the divider, along with the sensor
+// to make sure input voltage to the A0 pin never exceed 1V.
+double R_1Adj = 10000.0;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(therm_pin,INPUT);
-  analogReference(EXTERNAL);
+//  analogReference(EXTERNAL);
 }
 
 double C0 = 0.0009621996628; //0.9621996628 e-3
 double C1 = 0.0001800586745; //1.800586745 e-4
 double C2 = 0.000000197132282; //1.971322820 e-7 
 
-double factor = 10000000.0
+double factor = 10000000.0;
 double CM0 = 9621.996628; //0.9621996628 e-3
 double CM1 = 1800.586745; //1.800586745 e-4
 double CM2 = 1.97132282; //1.971322820 e-7 
 
+double sensorMax = 1.0;
+
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  double sensorValue = analogRead(therm_pin);
+
+  // read the pin value 10 times over 100 ms span and take average
+  double sensorValue = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    // read the input on analog pin 0:
+    sensorValue += analogRead(therm_pin);
+    delay(10);
+  }
+
+  sensorValue = sensorValue / 10;
 
   // Convert the analog reading (which goes from 0 - 1023) to voltage reference (3.3V or 5V or other):
-  double voltage = sensorValue * V_0 /1024.0;
+  double voltage = sensorValue * sensorMax /1023.0;
 
-  double rThermistor = R_1 * voltage/(V_0 - voltage); // voltage to resistance
+  // i'm adjusting the misterious temperature error (for now treating it as constant error)
+  voltage = voltage - 0.028;
+
+  double R_1 = R_2 * (V_0 - voltage)/voltage; // voltage to resistance
+
+  // remove the fixed 230k ohm on the vcc side of the voltage dividor
+  double rThermistor = R_1 - R_1d - R_1Adj;
+
+  //rThermistor = 133000;
   double lnR = log(rThermistor);
 
   // T(K) = 1/(C0 + C1Ln(R) + C2(Ln(R))^3)
   double temp  = factor /(CM0 + lnR * CM1 + CM2 * lnR * lnR * lnR);
-  temp  = T_sum - 273.15; // K -> C
+  temp  = temp - 273.15; // K -> C
     
   Serial.print("Thermistor: ");
 
   Serial.print("A0: ");
   Serial.println(sensorValue);
 
-  Serial.print("RT: ");
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  
+  Serial.print("RT (ohm): ");
   Serial.println(rThermistor);
 
-  Serial.print(T_sum);
+  Serial.print("TEMP (C): ");
+  Serial.print(temp);
   Serial.print(" (");
-  Serial.print((T_sum*9.0/5.0)+32.0);
+  Serial.print((temp*9.0/5.0)+32.0);
   Serial.println(" F)");
 
   delay(1500);
